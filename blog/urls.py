d88d74a6ddeb68fe416f308f7ffa5049bf469660
pooja-varmaa <varmapooja020@gmail.com>
2022-04-02 20:27:10 +0530

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('', views.post_list, name='post_list'),
+]
+
+
+
